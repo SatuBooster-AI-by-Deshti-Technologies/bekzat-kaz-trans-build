246b88c03f030e9af820f2d1a01d2e4b52198134
Assylzhan <120775947+Assylzhan09@users.noreply.github.com>
2025-07-22 15:55:16 +0500

    1cc

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -5,26 +5,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { MapPin, Phone, Mail, Clock } from "lucide-react";
 
 const contactInfo = [
-  {
-    icon: MapPin,
-    title: "Адрес",
-    details: ["г. Алматы, Райымбек батыр 375 д", "Казахстан"]
-  },
-  {
-    icon: Phone,
-    title: "Телефон",
-    details: ["+77071117425"]
-  },
-  {
-    icon: Mail,
-    title: "Email",
-    details: ["info@bekzatkts.kz", "order@bekzatkts.kz"]
-  },
-  {
-    icon: Clock,
-    title: "Режим работы",
-    details: ["Пн-Пт: 8:00 - 18:00", "Сб: 9:00 - 15:00"]
-  }
+
 ];
 
 const ContactSection = () => {
